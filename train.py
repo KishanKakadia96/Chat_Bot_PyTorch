b83fffc24ccea925b0cd09417962aa94fbd76eb5
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-29 19:31:35 +0200

    creating training dataset

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
-from src.utils.nltk_utils import tokenize,stem
+from src.utils.nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from touch.utils.data import Dataset,
 
 with open(r'C:\Kishan\Github\NLP\Chat_Bot_PyTorch\intents.json','r') as f:
     intents = json.load(f)
@@ -20,6 +24,37 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+        #dataset[idx]
+        def getitem(self):
+            return self.x_data[idx],self.y_data[idx]
+        def __len__(self):
+            return self.n_samples
+
+    #Hyperparameter
+    batch_size = 8
+
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
 
 
 
