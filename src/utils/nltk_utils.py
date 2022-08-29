e69d77ea43d793e39e421e527e1610b2620375e8
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-29 19:28:33 +0200

    Bag of words implemented

--- src/utils/nltk_utils.py
+++ src/utils/nltk_utils.py
@@ -1,6 +1,7 @@
 import nltk
 #nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -11,5 +12,12 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenize_sentence,all_words):
-    pass
+    tokenize_sentence = [stem(w) for w in tokenize_sentence]
+
+    bag = np.zeros(len(all_words),dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenize_sentence:
+            bag[idx] = 1.0
+
+    return bag
 
